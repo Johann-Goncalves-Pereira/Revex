dad2c544087d081e70f39cf43d1f3def66dd6c49
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-03-14 09:52:40 -0300

    build: :sparkles: add all styles files at once

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { Elm } from "./Main.elm";
 
+const modules = import.meta.globEager("./Styles/*.scss");
+
 const app = Elm.Main.init();
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
 import { defineConfig } from "vite";
 import elm from "vite-plugin-elm";
-import globalStyle from "@originjs/vite-plugin-global-style";
 
-export default {
-  plugins: [
-    elm(),
-    globalStyle({
-      sourcePath: "./src/Styles/",
-      lessEnabled: false,
-      stylusEnabled: false,
-    }),
-  ],
-  build: {
-    outDir: "build",
-    target: "es2020",
-  },
-};
+export default defineConfig({
+  plugins: [elm()],
+});
