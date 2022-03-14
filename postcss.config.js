3c014d3d1c36acc93f4d6fdd1173ae5180762050
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-03-14 10:22:52 -0300

    build: :zap: add postcss and open-props

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,22 @@ module.exports = {
   plugins: {
     autoprefixer: {},
     doiuse: {},
+    cssnano: {
+      preset: "cssnano-preset-default",
+    },
+    "postcss-color-alpha": {},
+    "postcss-custom-media": {},
+    "postcss-custom-selectors": {},
+    "postcss-inline-svg": {},
+    "postcss-jit-props": {
+      "open-props": {},
+    },
     "postcss-plugin": {},
     "postcss-preset-env": {},
+    "postcss-utilities": {},
+    "rucksack-css": {},
   },
 };
+
+//! Do I Use Config
+// browsers: ["ie >= 8", "> 1%"],
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Elm } from "./Main.elm";
 
-const modules = import.meta.globEager("./Styles/*.scss");
+const stylesL00 = import.meta.globEager("./Styles/*.scss");
+const stylesL01 = import.meta.globEager("./Styles/**/*.scss");
 
 const app = Elm.Main.init();
