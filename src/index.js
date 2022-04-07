ae45bde8bbfb8bc9ea4213f0638cd3295043c9d4
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-07 17:27:46 -0300

    feat: :fire: refactor reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Elm } from "./Main.elm";
 
-const stylesL00 = import.meta.globEager("./Styles/*.scss");
-const stylesL01 = import.meta.globEager("./Styles/**/*.scss");
+//Lazy
+const styles = import.meta.globEager("./Styles/**/*.scss");
+// import "./Styles/_index.scss";
 
 const app = Elm.Main.init();
