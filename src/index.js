1ce206a7d290e8907d68a016ef3b5d447885dbcf
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-25 16:39:25 -0300

    feat: :zap: no dubplicated css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Elm } from "./Main.elm";
 
-// Import the CSS files
+//* Import the CSS files
+//? Import css but need to have a index on the folder of the css files
 const stylesIndex = import.meta.globEager("./Styles/**/_index.scss");
 //? Cause duplicate css
 // const stylesEvery = import.meta.globEager("./Styles/**/*.scss");
