9c38124e71fee1f86fbf27864878cf163cde7377
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-25 16:37:33 -0300

    feat: :zap: no dubplicated css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Elm } from "./Main.elm";
 
 // Import the CSS files
-const styles = import.meta.globEager("./Styles/**/*.scss");
+const stylesIndex = import.meta.globEager("./Styles/**/_index.scss");
+//? Cause duplicate css
+// const stylesEvery = import.meta.globEager("./Styles/**/*.scss");
+
 // Initialize our Elm app
 const app = Elm.Main.init();
