b2aae3e20be0c956121c720e2b7f9dbeaf2d4b81
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-05-16 15:51:17 -0300

    feat: tailwind colors with props

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `hsl(var(${variable}))`;
+    }
+    return `hsl(var(${variable}), ${opacityValue})`;
+  };
+}
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,elm}"],
   theme: {
     colors: {
-      "surface-0": "#050606",
-      "surface-1": "#101215",
-      "surface-2": "#23282c",
-      "surface-3": "#353c43",
+      //& Surface
+      "surface-0": withOpacityValue("--clr-surface-0"),
+      "surface-1": withOpacityValue("--clr-surface-1"),
+      "surface-2": withOpacityValue("--clr-surface-2"),
+      "surface-3": withOpacityValue("--clr-surface-3"),
+      // & Text
+      "text-0": withOpacityValue("--clr-text-0"),
+      "text-1": withOpacityValue("--clr-text-1"),
+      "text-2": withOpacityValue("--clr-text-2"),
+      // & Brand
+      "brand-0": withOpacityValue("--clr-brand-0"),
     },
     extend: {},
   },
