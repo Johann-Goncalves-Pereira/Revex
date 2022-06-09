b8580089b3e7ea3f9eda2d6073f44a7233553d8b
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-06-09 18:26:10 -0300

    feat: on extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,elm}"],
   theme: {
-    colors: {
-      //& Surface
-      "surface-0": "hsla(var(--clr-surface-0-hsl) / <alpha-value>)",
-      "surface-1": "hsla(var(--clr-surface-1-hsl) / <alpha-value>)",
-      "surface-2": "hsla(var(--clr-surface-2-hsl) / <alpha-value>)",
-      "surface-3": "hsla(var(--clr-surface-3-hsl) / <alpha-value>)",
-      // & Text
-      "text-0": "hsla(var(--clr-text-0-hsl) / <alpha-value>)",
-      "text-1": "hsla(var(--clr-text-1-hsl) / <alpha-value>)",
-      "text-2": "hsla(var(--clr-text-2-hsl) / <alpha-value>)",
-      // & Brand
-      "brand-0": "hsla(var(--clr-brand-0-hsl) / <alpha-value>)",
+    // colors: {},
+    extend: {
+      colors: {
+        //& Surface
+        "surface-0": "hsla(var(--clr-surface-0-hsl) / <alpha-value>)",
+        "surface-1": "hsla(var(--clr-surface-1-hsl) / <alpha-value>)",
+        "surface-2": "hsla(var(--clr-surface-2-hsl) / <alpha-value>)",
+        "surface-3": "hsla(var(--clr-surface-3-hsl) / <alpha-value>)",
+        // & Text
+        "text-0": "hsla(var(--clr-text-0-hsl) / <alpha-value>)",
+        "text-1": "hsla(var(--clr-text-1-hsl) / <alpha-value>)",
+        "text-2": "hsla(var(--clr-text-2-hsl) / <alpha-value>)",
+        // & Brand
+        "brand-0": "hsla(var(--clr-brand-0-hsl) / <alpha-value>)",
+      },
     },
-    // extend: {},
   },
   plugins: [],
   corePlugins: {
