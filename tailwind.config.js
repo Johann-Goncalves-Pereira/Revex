d0c738285f48c1f5d5a3de24a61c0190c1c957a4
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-06-09 17:51:07 -0300

    feat: update tailwind and color alpha method

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,21 @@
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `hsl(var(${variable}))`;
-    }
-    return `hsla(var(${variable}), ${opacityValue})`;
-  };
-}
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,elm}"],
   theme: {
     colors: {
       //& Surface
-      "surface-0": withOpacityValue("--clr-surface-0-alpha"),
-      "surface-1": withOpacityValue("--clr-surface-1-alpha"),
-      "surface-2": withOpacityValue("--clr-surface-2-alpha"),
-      "surface-3": withOpacityValue("--clr-surface-3-alpha"),
+      "surface-0": "hsla(var(--clr-surface-0-hsl) / <alpha-value>)",
+      "surface-1": "hsla(var(--clr-surface-1-hsl) / <alpha-value>)",
+      "surface-2": "hsla(var(--clr-surface-2-hsl) / <alpha-value>)",
+      "surface-3": "hsla(var(--clr-surface-3-hsl) / <alpha-value>)",
       // & Text
-      "text-0": withOpacityValue("--clr-text-0-alpha"),
-      "text-1": withOpacityValue("--clr-text-1-alpha"),
-      "text-2": withOpacityValue("--clr-text-2-alpha"),
+      "text-0": "hsla(var(--clr-text-0-hsl) / <alpha-value>)",
+      "text-1": "hsla(var(--clr-text-1-hsl) / <alpha-value>)",
+      "text-2": "hsla(var(--clr-text-2-hsl) / <alpha-value>)",
       // & Brand
-      "brand-0": withOpacityValue("--clr-brand-0-alpha"),
+      "brand-0": "hsla(var(--clr-brand-0-hsl) / <alpha-value>)",
     },
-    extend: {},
+    // extend: {},
   },
   plugins: [],
   corePlugins: {
