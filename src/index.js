6d1b34df6942d069aa07398c20b6cde684bbaf30
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-06-10 15:50:44 -0300

    feat: new sass structure imports

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { Elm } from "./Main.elm";
 
 //* Import the CSS files
 //? Import css but need to have a index on the folder of the css files
-const stylesIndex = import.meta.globEager("./Styles/**/_index.scss");
+// const stylesIndex = import.meta.globEager("./Styles/**/_index.scss");
 //? Cause duplicate css
 // const stylesEvery = import.meta.globEager("./Styles/**/*.scss");
+import './Styles/_index.scss';
 
 // Initialize our Elm app
 const app = Elm.Main.init();
