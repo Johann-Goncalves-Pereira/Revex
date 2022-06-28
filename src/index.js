95d61f4cf18b908c9b4de4d5c4283483d4b0de2b
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-06-28 17:13:46 -0300

    feat: no useless indexetion required

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { Elm } from "./Main.elm";
 
-//* Import the CSS files
 //? Import css but need to have a index on the folder of the css files.
 // const stylesIndex = import.meta.globEager("./Styles/**/_index.scss");
-//? Cause duplicate css.
-// const stylesEvery = import.meta.globEager("./Styles/**/*.scss");
 //? Just import the index.scss no side effect, but need to import everything manually.
-import "./Styles/_index.scss";
+// import "./Styles/_index.scss";
+//? Import every css on the Styles folder.
+const stylesEvery = import.meta.globEager("./Styles/**");
 
 // Initialize our Elm app
 const app = Elm.Main.init();
