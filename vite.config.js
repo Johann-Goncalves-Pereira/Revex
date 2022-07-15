ea6b64bc028e532e927d52938cd7218cd9b37277
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-07-15 11:41:20 -0300

    build: env config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+
 import elm from "vite-plugin-elm";
 import legacy from "@vitejs/plugin-legacy";
 
-export default defineConfig({
-  plugins: [
-    elm(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 8080,
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      elm(),
+      legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+    ],
+    server: {
+      port: 3000,
+    },
+    preview: {
+      port: 8080,
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  };
 });
