48657f516dc9b9d7ddbe7a218a88f0a565a9d439
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-08-11 22:24:04 -0300

    feat: just mode

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from "vite";
 import elm from "vite-plugin-elm";
 import legacy from "@vitejs/plugin-legacy";
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
