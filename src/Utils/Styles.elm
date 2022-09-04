976b8b72560d4c51bd407c4632720c4326841db4
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-09-04 15:37:13 -0300

    feat: style

---
+++ src/Utils/Styles.elm
@@ -0,0 +1,15 @@
+module Utils.Styles exposing (..)
+
+import Html exposing (Attribute)
+import Html.Attributes exposing (attribute)
+
+
+customProps : List { prop : String, value : String } -> Attribute msg
+customProps listProps =
+    List.foldl
+        (\{ prop, value } result ->
+            String.concat [ result, "--", prop, ":", value, ";" ]
+        )
+        ""
+        listProps
+        |> attribute "style"
