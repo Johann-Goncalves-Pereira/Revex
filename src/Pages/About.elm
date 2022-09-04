3c52a85938b9fccdf73e1ccdf62b65e4f5c72451
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-09-04 15:36:50 -0300

    feat: Shared

--- src/Pages/About.elm
+++ src/Pages/About.elm
@@ -1,24 +1,26 @@
 module Pages.About exposing (Model, Msg, page)
 
+import Components.Layout as Layout exposing (initLayout)
 import Gen.Params.About exposing (Params)
 import Gen.Route as Route
 import Html exposing (Html, button, div, em, h1, input, p, section, text)
 import Html.Attributes exposing (class, id, value)
 import Html.Attributes.Aria exposing (ariaLabelledby)
 import Html.Events exposing (onClick, onInput)
-import Layout exposing (initLayout)
 import Page
+import Ports.Storage as Storage exposing (Storage)
 import Request
 import Shared
 import View exposing (View)
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
-page shared req =
-    Page.sandbox
+page shared _ =
+    Page.element
         { init = init
-        , update = update
-        , view = view
+        , update = update shared
+        , view = view shared
+        , subscriptions = \_ -> Sub.none
         }
 
 
@@ -27,12 +29,14 @@ page shared req =
 
 
 type alias Model =
-    { inputValue : String, intValue : Int }
+    {}
 
 
-init : Model
+init : ( Model, Cmd Msg )
 init =
-    { inputValue = "", intValue = 0 }
+    ( {}
+    , Cmd.none
+    )
 
 
 
@@ -41,42 +45,38 @@ init =
 
 type Msg
     = ChangeInput String
-    | AddValue
-    | SubtractValue
+    | ChangeInt (Int -> Int -> Int)
 
 
-update : Msg -> Model -> Model
-update msg model =
+update : Shared.Model -> Msg -> Model -> ( Model, Cmd Msg )
+update shared msg model =
     case msg of
         ChangeInput newString ->
-            { model | inputValue = newString }
+            ( model, Storage.changeString shared.storage newString )
 
-        AddValue ->
-            { model | intValue = model.intValue + 1 }
-
-        SubtractValue ->
-            { model | intValue = model.intValue - 1 }
+        ChangeInt v_ ->
+            ( model, Storage.changeInt shared.storage v_ )
 
 
 
 -- VIEW
 
 
-view : Model -> View Msg
-view model =
+view : Shared.Model -> Model -> View Msg
+view shared model =
     { title = "Revex - About"
     , body =
         Layout.viewLayout
             { initLayout
                 | route = Route.About
                 , mainAttrs = [ class "flex justify-center items-center" ]
-                , mainContent = [ viewPlaceholder model ]
+                , mainContent = [ viewPlaceholder shared model ]
             }
     }
 
 
-viewPlaceholder : Model -> Html Msg
-viewPlaceholder model =
+viewPlaceholder : Shared.Model -> Model -> Html Msg
+viewPlaceholder shared _ =
     section [ class "bg-surface-200 p-8 rounded-xl w-[min(100vw_-_3rem,42rem)]", ariaLabelledby "heading" ]
         [ h1 [ class "text-2 font-500 mb-3", id "heading" ] [ text "About" ]
         , p [ class "text-1 opacity-70" ]
@@ -86,16 +86,16 @@ viewPlaceholder model =
             ]
         , div [ class "relative" ]
             [ p [ class "absolute left-2 top-1/2 -translate-y-1/2  text-[#ffffff86] pointer-events-none select-none text-1" ] [ text "Input value: " ]
-            , input [ class "bg-surface-100 p-2 rounded-lg w-full pl-20 border-0 my-5 h-8 text-[0.875rem]", onInput ChangeInput, value model.inputValue ] []
+            , input [ class "bg-surface-100 p-2 pl-[13ch] rounded-lg w-full border-0 my-5 h-8 text-[0.875rem]", onInput ChangeInput, value shared.storage.string ] []
             ]
         , div [ class "grid grid-cols-[max-content,1fr] items-center bg-surface-100 p-2 rounded-lg mb-5 w-full h-8" ]
             [ em [ class "text-xs text-[#ffffff86] pr-2 pointer-events-none select-none text-1" ] [ text "Inverted value: " ]
-            , p [ class "overflow-hidden text-1" ] [ String.reverse model.inputValue |> text ]
+            , p [ class "overflow-hidden text-1" ] [ String.reverse shared.storage.string |> text ]
             ]
         , div [ class "flex flex-col items-center justify-center gap-5 sm:flex-row" ]
-            [ button [ class "w-full bg-surface-100 px-6 py-2 rounded-lg sm:w-auto", onClick SubtractValue ] [ text "-" ]
+            [ button [ class "w-full bg-surface-100 px-6 py-2 rounded-lg sm:w-auto", onClick <| ChangeInt (-) ] [ text "-" ]
             , p [ class "bg-surface-100 px-6 py-2 rounded-lg w-full text-center pointer-events-none select-none cursor-default" ]
-                [ text <| String.fromInt model.intValue ]
-            , button [ class "w-full bg-surface-100 px-6 py-2 rounded-lg sm:w-auto", onClick AddValue ] [ text "+" ]
+                [ text <| String.fromInt shared.storage.int ]
+            , button [ class "w-full bg-surface-100 px-6 py-2 rounded-lg sm:w-auto", onClick <| ChangeInt (+) ] [ text "+" ]
             ]
         ]
