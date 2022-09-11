a8c502b8de0ccea93cbf23b2e6256070eb61757b
Johann-Goncalves-Pereira <johann.gon.pereira@gmail.com>
2022-09-11 18:51:00 -0300

    feat: try and catch

--- src/Ts/Ports/storage.ts
+++ src/Ts/Ports/storage.ts
@@ -5,8 +5,12 @@ export const StorageFlag: JSON = JSON.parse(
 );
 
 export function Storage(app: Elm) {
-  app.ports.save.subscribe((storage: JSON) => {
-    localStorage.setItem("storage", JSON.stringify(storage));
-    app.ports.load.send(storage);
-  });
+  try {
+    app.ports.save.subscribe((storage: JSON) => {
+      localStorage.setItem("storage", JSON.stringify(storage));
+      app.ports.load.send(storage);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
